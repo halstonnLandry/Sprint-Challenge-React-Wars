7f00763552a9f85022ba37c81762543fc2b26196
Halstonn Landry <hallrstein@gmail.com>
2020-03-20 13:14:17 -0500

    incomplete

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import styled from 'styled-components';
 
 const App = () => {
+    const [data, setData]= useState ([]);
+   useEffect(()=>{
+    axios.get("https://swapi.co/api/people")
+    .then(response=>{
+      setData(response.data);
+      console.log(response.data);
+    })
+    .catch(error=>{
+      console.log(error);
+    });
+   },[])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,9 +22,18 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+const H1= styled.h1`
+font-size:100px;
+color:#ffff00;
+background-color:#00000090;
+box-shadow: 0 0 25px #ffff00;
+border-radius: 20px;
+margin: 20px;
+`
   return (
+
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <H1 className="Header">React Wars</H1>
     </div>
   );
 }
